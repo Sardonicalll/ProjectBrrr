79f82a18f241abb5718c28a6060ecab6314136c7
Dimmerworld <Quadcorenerd@gmail.com>
2018-08-14 17:25:45 +1000

    Character movement

--- Project TS/Assets/Scripts/Character/Movement.cs
+++ Project TS/Assets/Scripts/Character/Movement.cs
@@ -4,13 +4,36 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+   public MeshRenderer a;
+    public ParticleSystem ps;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            a.enabled = false;
+            ps.Play(true);
+            Invoke("warp", 0.2f);
+        }
+        else
+        {
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 300.0f;
+            var z = Input.GetAxis("Vertical") * Time.deltaTime * 6.0f;
+
+            transform.Rotate(0, x, 0);
+            transform.Translate(0, 0, z);
+        }
+    }
+
+    void warp()
+    {
+        ps.Stop(true);
+        transform.Translate(0, 0, 20);
+        a.enabled = true;
+    }
 }
