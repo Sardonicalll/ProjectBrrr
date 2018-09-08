ba2848279efd401a29b69e8a496712057d20d8a3
William <s3719681@student.rmit.edu.au>
2018-09-09 00:01:15 +1000

    Pugatoo animation and AI

---
+++ Project TS/Assets/Scripts/AI/PugatooAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+
+    public Animator animator;
+    public Transform player;
+    public Rigidbody rigid;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rigid.velocity.magnitude>0)
+        {
+            animator.SetBool("walking", true);
+        }
+        else
+        {
+            animator.SetBool("walking", false);
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, .03f);
+    }
+}
