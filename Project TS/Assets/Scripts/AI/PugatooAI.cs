4ab2a212154f5dd56ce135c8804e14fd5323a632
Dimmerworld <Quadcorenerd@gmail.com>
2018-09-09 01:58:14 +1000

    Prefabs and Pugatoo AI work

--- Project TS/Assets/Scripts/AI/PugatooAI.cs
+++ Project TS/Assets/Scripts/AI/PugatooAI.cs
@@ -9,6 +9,9 @@ public class PugatooAI : MonoBehaviour {
     public Transform player;
     public Rigidbody rigid;
 
+    public float Speed = .03f;
+    public float lookDelay = 1;
+
 
 	// Use this for initialization
 	void Start () {
@@ -17,15 +20,37 @@ public class PugatooAI : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (rigid.velocity.magnitude>0)
+
+        Vector3 target = player.transform.position;
+        if (Vector3.Distance(transform.position, target) > 2 )
         {
             animator.SetBool("walking", true);
+            transform.position = Vector3.MoveTowards(transform.position, target, Speed);
         }
         else
         {
             animator.SetBool("walking", false);
         }
 
-        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, .03f);
+        var lookPos = target - transform.position;
+        lookPos.y = 0;
+        var rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, lookDelay);
+
     }
+
+
+
+    // Slow turn (Maybe)
+    //Vector3 DirectionXZ()
+    //{
+    //    Quaternion targetRotation = Quaternion.LookRotation(DirectionXZ());
+    //    transform.rotation = Quaternion.RotateTowards(transform.rotation,
+    //        targetRotation, 40 * Time.deltaTime);
+
+    //    Vector3 direction = player.position - transform.position;
+    //    direction.y = 0; // Ignore Y
+    //    return direction;
+    //}
 }
+
