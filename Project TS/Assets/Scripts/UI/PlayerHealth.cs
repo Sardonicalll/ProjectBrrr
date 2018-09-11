3b449f04f2eaee3c120c3f6a335e2bf1d85c57a5
Unknown <s3717184@student.rmit.edu.au>
2018-09-11 14:13:09 +1000

    Basic health and stamina
    Press k to kys, press space to tele and reduce stam (There needs to be a if statement in the player movement to check if stam is > 0 )

---
+++ Project TS/Assets/Scripts/UI/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+public int maxhealth;
+private int currenthealth;
+private int takendmg;
+public Text hptxt;
+
+public Text death;
+
+	void Start () {
+		maxhealth = 100;
+		takendmg = 0;
+		currenthealth =  maxhealth;
+		hptxt.text = "HP: " + currenthealth.ToString();
+	}
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+			currenthealth = currenthealth - 10;
+            hptxt.text = "HP:" + currenthealth.ToString();
+        }
+
+	if (currenthealth <= 0) {
+		death.text = "REEEEEEEEEEEEEEEEE U DEAD";
+
+	}
+	}
+}
---
+++ Project TS/Assets/Scripts/UI/PlayerStam.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStam : MonoBehaviour {
+
+public int maxstam;
+public int currentstam;
+private int stamloss;
+
+public Text stamtxt;
+
+	// Use this for initialization
+	void Start () {
+		currentstam = maxstam;
+        stamtxt.text = "Stam:" + currentstam.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (currentstam > 0 )
+		{
+        	if (Input.GetKeyDown(KeyCode.Space))
+       	 	{
+			stamloss = 10;
+            currentstam = currentstam - stamloss;
+            stamtxt.text = "Stam:" + currentstam.ToString();
+      	 	}
+		}
+	}
+}
