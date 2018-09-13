5ceabf27186a72d9ff40f7df88bf8be5b66262b2
Dimmerworld <Quadcorenerd@gmail.com>
2018-09-14 01:55:30 +1000

    Fix UIController conflict and prefab update.

--- Project TS/Assets/Scripts/UI/UIController.cs
+++ Project TS/Assets/Scripts/UI/UIController.cs
@@ -1,16 +1,23 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class HudUpdater : MonoBehaviour {
+public class UIController : MonoBehaviour {
 
     PlayerHealth ph;
     PlayerStamina ps;
 
-    public Text hpT;
-    public Text stT;
+    public RawImage dhBG;
+    public Text dhT;
 
-	// Use this for initialization
-	void Start () {
+    public Text hpT;
+    public Slider hpS;
+
+    public Text stT;
+    public Slider stS;
+   
+
+    // Use this for initialization
+    void Start () {
         GameObject player = GameObject.FindWithTag("Player");
         ph = player.GetComponent<PlayerHealth>();
         ps = player.GetComponent<PlayerStamina>();
@@ -18,7 +25,22 @@ public class HudUpdater : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        hpT.text = "HP: " + ph.GetHealth().ToString();
-        stT.text = "Stamina: " + ps.GetStamina().ToString();
+        float health = ph.GetHealth();
+        float stamina = ps.GetStamina();
+        float maxHealth = ph.GetMaxHealth();
+        float maxStamina = ps.GetMaxStamina();
+
+        hpT.text = ((health/maxHealth)*100).ToString() + "%";
+        stT.text = ((stamina / maxStamina) * 100).ToString() + "%";
+
+        hpS.value = 1 - (health / maxHealth);
+        stS.value = 1 - (stamina / maxStamina);
+
+
+        if(health <= 0)
+        {
+            dhT.text = "GIT GUD SCRUB";
+            dhBG.enabled = true;
+        }
 	}
 }
